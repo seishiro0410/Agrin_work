8fc8e0bb28b18a072c61af283d1d38343c1a0d6c
Seishiro Fukuda <108728183+seishiro0410@users.noreply.github.com>
2022-11-28 14:29:32 +0000

    [add]customer costomer show

--- app/controllers/customer/customers_controller.rb
+++ app/controllers/customer/customers_controller.rb
@@ -1,7 +1,21 @@
 class Customer::CustomersController < ApplicationController
   def show
+    @customer = current_customer
   end
 
   def edit
+    @customer = current_customer
+  end
+
+  def update
+    @customer = current_customer
+    @customer.update(customer_params)
+    redirect_to customers_mypage_path
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :gender, :age, :postal_code, :address, :telephone_number, :email, :customers_image)
   end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -7,7 +7,18 @@ class Customer < ApplicationRecord
   has_many :reservations, dependent: :destroy
   has_many :customer_reviews, dependent: :destroy
   has_many :farmer_reviews, dependent: :destroy
-  has_one_attached :image
+
+  has_one_attached :customers_image
 
   enum gender: { man: 0, woman: 1 }
+
+  has_one_attached :profile_image
+
+  def get_customers_image
+    unless customers_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      customers_image.attach(io: File.open(file_path), filename: 'no_image.jpg', content_type: 'image/jpeg')
+    end
+    customers_image
+  end
 end
