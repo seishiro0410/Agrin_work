e2e74b0108bee0e89ee175d0835914b63c0e5bc7
Seishiro Fukuda <108728183+seishiro0410@users.noreply.github.com>
2023-01-14 13:07:48 +0000

    fix farmer sign_up view customer sign_up view

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Admin.create!(
-   email: 'seishiro0410@gmail.com',
-   password: 'se4i8shi6ro9'
-)
+# Admin.create!(
+#    email: 'seishiro0410@gmail.com',
+#    password: 'se4i8shi6ro9'
+# )
+
+admins = [
+   {email: 'seishiro0410@gmail.com', password: 'se4i8shi6ro9'},
+]
+
+admins.each do |admin|
+   admin_data = Admin.find_by(email: admin[:email])
+   if admin_data.nil?
+      Admin.create(email: admin[:email], password: admin[:password])
+   end
+end
